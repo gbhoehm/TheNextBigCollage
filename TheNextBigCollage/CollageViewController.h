da0e0f790dd12b5783b02eb5d3a21675a5b50536
Your Name <marcusnj222@gmail.com>
2018-10-11 10:17:13 -0500

    Add collage view controller.

---
+++ TheNextBigCollage/CollageViewController.h
@@ -0,0 +1,17 @@
+//
+//  CollageViewController.h
+//  TheNextBigCollage
+//
+//  Created by Marcus Gubanyi on 10/11/18.
+//  Copyright © 2018 Concordia Software Class. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CollageViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TheNextBigCollage/CollageViewController.m
@@ -0,0 +1,32 @@
+//
+//  CollageViewController.m
+//  TheNextBigCollage
+//
+//  Created by Marcus Gubanyi on 10/11/18.
+//  Copyright © 2018 Concordia Software Class. All rights reserved.
+//
+
+#import "CollageViewController.h"
+
+@interface CollageViewController ()
+
+@end
+
+@implementation CollageViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
