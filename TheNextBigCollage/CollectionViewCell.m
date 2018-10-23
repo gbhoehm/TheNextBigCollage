6cb83b65d9b518d91b7a10c2e34269a29986ab5d
Marlene Maier <marlenemaier@std-0439-1.cune.pri>
2018-10-23 10:06:57 -0500

    Customized collection view cells by adding a picture and a title in each cell.

---
+++ TheNextBigCollage/CollectionViewCell.m
@@ -0,0 +1,15 @@
+//
+//  CollectionViewCell.m
+//  TheNextBigCollage
+//
+//  Created by Marlene Maier on 10/23/18.
+//  Copyright © 2018 Concordia Software Class. All rights reserved.
+//
+
+#import "CollectionViewCell.h"
+
+@implementation CollectionViewCell
+
+
+
+@end
